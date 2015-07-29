bb6a820f915ec6f78a77931b5577fb59c3f2693a
laurenmlang <laurenmlang@gmail.com>
2015-07-29 15:12:11 -0500

    Final Version

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,32 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+  	@id = params["id"]
+
+  	# return the picture for the id
+  	@record = Photo.find_by({ :id => @id})
+  	@source = @record.source
+  	@caption = @record.caption
+
+  	render("show.html.erb")
+  end
+
+  def destroy
+   	@id = params["id"]
+
+
+  	# return the picture for the id
+  	@record = Photo.find(@id) 	
+
+  	@del = @record.destroy
+
+  	render("destroy.html.erb")
+  end
+
+  def new_form
+  	render("new_form.html.erb")
+  end
 
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,16 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  # Route to create new record
+  get("/photos/new", { :controller => "photos", :action => "new_form" })
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
 
+  # Route to delete photo
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+
+
 end
+
